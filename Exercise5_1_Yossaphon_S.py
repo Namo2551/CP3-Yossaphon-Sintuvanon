82c026d69cef4f352911def37fb9cf986296951d
Namo2551 <monkeydnamo@gmail.com>
2025-05-24 16:24:15 +0700

    24/5/68 Exercise5

---
+++ Exercise5_1_Yossaphon_S.py
@@ -0,0 +1,7 @@
+F_number = float(input("กรอกเลขตัวเเรก : "))
+S_number = float(input("กรอกเลขตัวที่สอง : "))
+
+print(F_number, "+", S_number, "=", F_number + S_number)
+print(F_number, "-", S_number, "=", F_number - S_number)
+print(F_number, "*", S_number, "=", F_number * S_number)
+print(F_number, "/", S_number, "=", F_number / S_number)
---
+++ Exercise_5_2_Yossaphon_S.py
@@ -0,0 +1,4 @@
+distant =  int(input("กรอกระยะทาง : "))
+time = int(input("กรอกเวลา : "))
+velocity = distant / time
+print(velocity, "m/s")
